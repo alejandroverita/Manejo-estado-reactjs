6ea63f0816796d654a45a5a87b247295df0f7526
avera6756 <avera6756@utm.edu.ec>
2021-12-16 14:47:33 -0500

    [UPDATE] Action creators and actionTypes]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { UseState } from './UseState.js';
+import { UseReducer } from './UseReducer.js';
 import { ClassState } from './ClassState.js';
+import { UseState } from './UseState.js';
+
 
 import './App.css';
 
@@ -7,6 +9,7 @@ function App() {
   return (
     <div className="App">
      <UseState name='UseState'/>
+     <UseReducer name='UseReducer'/>
      <ClassState name='ClassState'/>
     </div>
   );
--- src/UseReducer.js
+++ src/UseReducer.js
@@ -1,3 +1,101 @@
+import React from 'react';
+
+
+const SECURITY_CODE = 'PARADIGMA';
+
+const UseReducer = ({name}) => {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+
+    console.log(state);
+
+    const onLoading = () => dispatch({type: actionTypes.LOADING})
+    const onConfirm = () => dispatch({type: actionTypes.CONFIRM})
+    const onError = () => dispatch({type: actionTypes.ERROR})
+    const onCheck = () => dispatch({type: actionTypes.CHECK})
+    const onDelete = () => dispatch({type: actionTypes.DELETE})
+    const onReset = () => dispatch({type: actionTypes.RESET})
+
+    const onWrite =({target})=>{
+        dispatch({type: actionTypes.WRITE, payload: target.value})
+    }
+
+
+    //Dos argumentos en useEffect, 1. Funcion 2. Cuando se ejecuta la funcion
+    React.useEffect(() => {
+        console.log('Empezando el efecto');
+
+        if(state.loading){
+            onLoading();
+            setTimeout(() => {
+                console.log('Haciendo validacion');
+                
+                if(state.value === SECURITY_CODE){
+                    onConfirm();
+                }else{
+                    onError();
+                } 
+                
+                console.log('Haciendo validacion');
+            }, 3000);
+        }
+        
+        console.log('Terminando el efecto');
+    }, [state.loading]);
+    
+
+    if(!state.deleted && !state.confirmed){
+        return (
+            <div>
+                <h2>Eliminar {name}</h2>
+                <p> Por favor, escribe el codigo de seguridad</p>
+    
+                {state.error && (
+                    <p>Error: El estado es incorrecto</p>
+                )}
+    
+                {state.loading && (
+                    <p>Cargando...</p>
+                )}
+    
+               {/*  {message && (
+                    <p className = {error ? 'text-red-400 ' : 'text-green-500'}>
+                        {message}
+                    </p>
+                )} */}
+                
+                <input 
+                    placeholder = 'Codigo de seguridad' type="text" 
+                    value={state.value}
+                    onChange = { onWrite} />
+                <button onClick={onCheck}>Comprobar</button>
+            </div>
+        );
+    } else if (state.confirmed && !state.deleted){
+        return (
+            <div>
+                <p>Pedimos confirmacion. Estas seguro?</p>
+                <button onClick={onDelete}>
+                    Si, eliminar
+                </button>
+                <button onClick={onReset} >
+                    No, no lo elimines.
+                </button>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <p>Eliminado con exito</p>
+                <button onClick={onReset}>
+                    Resetealo, varon
+                </button>
+            </div>
+        )
+    }
+}
+
+
+
 //Para un reducer necesitamos estados compuestos
 const initialState = {
     value: '',
@@ -54,27 +152,64 @@ const reducerSWITCH = (state, action) => {
     }
 }
 
+const actionTypes = {
+    LOADING: 'LOADING',
+    CONFIRM: 'CONFIRM',
+    CHECK: 'CHECK',
+    WRITE: 'WRITE',
+    ERROR: 'ERROR',
+    DELETE: 'DELETE',
+    RESET: 'RESET',
+};
+
 //3era forma usando OBJETOS
-const reducerOBJECT = (state) => ({
-    'ERROR': {
+const reducerOBJECT = (state, payload) => ({
+    [actionTypes.LOADING]:{
+        ...state,
+        loading: true,
+        error: false,
+    },
+
+    [actionTypes.CONFIRM]: {
+        ...state,
+        error: false,
+        loading: false,
+        confirmed: true
+    },
+
+    [actionTypes.CHECK]: {
+        ...state,
+        loading: true,
+    },
+    
+    [actionTypes.WRITE]: {
+        ...state,
+        // error: true,
+        // loading: false,
+        value: payload,
+    },
+    
+    [actionTypes.ERROR]: {
         ...state,
         error: true,
         loading: false,
     },
-    'CONFIRM': {
+    [actionTypes.DELETE]: {
         ...state,
-        error: false,
-        loading: false,
-        confirmed: true,
+        deleted: true,
+    },
+
+    [actionTypes.RESET]: {
+        ...state,
+        confirmed: false,
+        deleted: false,
+        value: '',
     },
 })
   
 const reducer = (state, action) => {
-    if (reducerOBJECT(state)[action.type]) {
-        return reducerOBJECT(state)[action.type];
-    } else {
-        return {
-            ...state,
-        }
-    }
+    return reducerOBJECT(state, action.payload)[action.type] || state;
 }
+
+
+export { UseReducer };
